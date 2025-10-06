edaad9e4aaeebb3be5a69cfe44d57d3a67add3cc
Clément Lefevre--Scoazec <lefevrescoazec@gmail.com>
2025-10-06 11:26:03 +0200

    le mot est maintenant trouvable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
-use std::fs;
+use std::{fs, io};
 use rand::RngCore;
 
 fn main() {
     let mot = lire_fichier();
     println!("{}", mot);
+    let separer = mot.split(":").collect::<Vec<&str>>();
+    let mot = separer[0];
+    let def = separer[1];
+
+    //println!("{}", mot + def);
+    println!("{}", def);
+    essai(mot,def);
+
+
+
+}
+
+fn essai(mot: &str,def: &str) {
+    let mut saisie = String::new();
+    loop {
+        println!("{}", def);
+        saisie.clear();
+        io::stdin()
+            .read_line(&mut saisie)
+            .expect("Erreur lors de la lecture");
+        if saisie.trim().to_lowercase()   == mot.trim().to_lowercase() {
+            println!("bravo tu as trouver !");
+            break;
+        }
+        println!("{}", saisie.trim().to_lowercase());
+        println!("{}",mot.trim());
+    }
 }
 
 fn lire_fichier() -> String {
