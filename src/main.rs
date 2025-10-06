29705fdd9290642e4ae20ab2bbeda5273017288f
Clément Lefevre--Scoazec <lefevrescoazec@gmail.com>
2025-10-06 13:52:28 +0200

    fonctionnel

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,11 @@ fn main() {
         autorisation = mot.1;
         let mot = separer[0];
         let def = separer[1];
-        if essai(mot,def) == true{
+        if essai(mot,def) == false || autorisation.len() == 0 {
+            println!("fini !");
             break;
         }
     }
-
-
 
 }
 
@@ -50,10 +49,14 @@ fn essai(mot: &str,def: &str) -> bool{
             sortie = "abandon";
             break;
         }
-        //println!("{}", saisie.trim().to_lowercase());
 
     }
-    true
+    if sortie == "abandon"{
+        false
+    }
+    else {
+        true
+    }
 
 }
 
@@ -61,17 +64,11 @@ fn choisi_mot(mut autorisation:Vec<&str>) -> (&str,Vec<&str>) {
     let mut aleatoire = rand::rng();
     let mut int_aleatoire = aleatoire.next_u32();
 
-
-    //let lignes: Vec<&str> = fichier.lines().collect();
-    // l//et nb_lignes = lignes.len();
-
-
     int_aleatoire = int_aleatoire % (autorisation.len()-1) as u32;
     let mot = autorisation[int_aleatoire as usize].clone();
 
     autorisation.retain(|m| m != &mot);
 
-    //interdiction.push(int_aleatoire as usize);
     (mot,autorisation)
 
 
