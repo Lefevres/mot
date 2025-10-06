cb2776fdd5e3869d58ee082659c0856e546270bc
Clément Lefevre--Scoazec <lefevrescoazec@gmail.com>
2025-10-06 10:41:05 +0200

    debut lecture fichier

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
+
+    lire_fichier();
     println!("Hello, world!");
+}
+
+fn lire_fichier() {
+    let fichier = fs::read_to_string("mot.txt");
+
+    for mot in fichier.unwrap().lines() {
+        println!("{:?} \n", mot);
+    }
 }
