9d4f52fe9a92d73263d3df2de45b886ccc264ef2
Clément Lefevre--Scoazec <lefevrescoazec@gmail.com>
2025-10-06 12:15:29 +0200

    des truc

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,77 @@ use std::{fs, io};
 use rand::RngCore;
 
 fn main() {
-    let mot = lire_fichier();
-    println!("{}", mot);
-    let separer = mot.split(":").collect::<Vec<&str>>();
-    let mot = separer[0];
-    let def = separer[1];
+    let fichier = fs::read_to_string("mot.txt").unwrap();
+    let mut autorisation: Vec<&str> = vec!();
+    let prep_autorisation:Vec<&str> = fichier.lines().collect();
+    for element in  prep_autorisation {
+        autorisation.push(element)
+    }
 
-    //println!("{}", mot + def);
-    println!("{}", def);
-    essai(mot,def);
+    loop {
+        let mot = choisi_mot(autorisation);
+        let separer = mot.0.split(":").collect::<Vec<&str>>();
+        autorisation = mot.1;
+        let mot = separer[0];
+        let def = separer[1];
+        if essai(mot,def) == true{
+            break;
+        }
+    }
 
 
 
 }
 
-fn essai(mot: &str,def: &str) {
+
+
+fn essai(mot: &str,def: &str) -> bool{
     let mut saisie = String::new();
+    let mut sortie ="";
     loop {
-        println!("{}", def);
+        println!("Definition : {}", def);
         saisie.clear();
         io::stdin()
             .read_line(&mut saisie)
             .expect("Erreur lors de la lecture");
         if saisie.trim().to_lowercase()   == mot.trim().to_lowercase() {
             println!("bravo tu as trouver !");
+            sortie = "trouver";
             break;
         }
-        println!("{}", saisie.trim().to_lowercase());
-        println!("{}",mot.trim());
+
+
+        if saisie.trim().to_lowercase() == "indice"{
+         println!("c'est un mot de {} lettres", mot.len());
+        }
+
+        if saisie.trim().to_lowercase() == "stop"{
+            sortie = "abandon";
+            break;
+        }
+        //println!("{}", saisie.trim().to_lowercase());
+
     }
+    true
+
 }
 
-fn lire_fichier() -> String {
+fn choisi_mot(mut autorisation:Vec<&str>) -> (&str,Vec<&str>) {
     let mut aleatoire = rand::rng();
     let mut int_aleatoire = aleatoire.next_u32();
-    let fichier = fs::read_to_string("mot.txt").unwrap();
-    let lignes: Vec<&str> = fichier.lines().collect();
 
-    let nb_lignes = lignes.len();
-    int_aleatoire = int_aleatoire % (nb_lignes-1) as u32;
-    println!("{}", int_aleatoire);
-    lignes[int_aleatoire as usize].to_string()
+
+    //let lignes: Vec<&str> = fichier.lines().collect();
+    // l//et nb_lignes = lignes.len();
+
+
+    int_aleatoire = int_aleatoire % (autorisation.len()-1) as u32;
+    let mot = autorisation[int_aleatoire as usize].clone();
+
+    autorisation.retain(|m| m != &mot);
+
+    //interdiction.push(int_aleatoire as usize);
+    (mot,autorisation)
 
 
 }
