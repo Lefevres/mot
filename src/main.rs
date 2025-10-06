e91ee2a5f0d504212be87c1aa0d69ff00b0d17ca
Clément Lefevre--Scoazec <lefevrescoazec@gmail.com>
2025-10-06 11:07:01 +0200

    maintenant le programme choisit un mot et sa définition au hazard

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::fs;
+use rand::RngCore;
 
 fn main() {
-
-    lire_fichier();
-    println!("Hello, world!");
+    let mot = lire_fichier();
+    println!("{}", mot);
 }
 
-fn lire_fichier() {
-    let fichier = fs::read_to_string("mot.txt");
+fn lire_fichier() -> String {
+    let mut aleatoire = rand::rng();
+    let mut int_aleatoire = aleatoire.next_u32();
+    let fichier = fs::read_to_string("mot.txt").unwrap();
+    let lignes: Vec<&str> = fichier.lines().collect();
 
-    for mot in fichier.unwrap().lines() {
-        println!("{:?} \n", mot);
-    }
+    let nb_lignes = lignes.len();
+    int_aleatoire = int_aleatoire % (nb_lignes-1) as u32;
+    println!("{}", int_aleatoire);
+    lignes[int_aleatoire as usize].to_string()
+
+
 }
