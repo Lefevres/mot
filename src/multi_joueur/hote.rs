cd9f0c7f32f736d03a1991cc3d0feb397f59f79b
Lefevres <lefevrescoazec@gmail.com>
2025-11-19 21:56:59 +0100

    début de hote qui attend maintenant des connexions

--- src/multi_joueur/hote.rs
+++ src/multi_joueur/hote.rs
@@ -1,9 +1,58 @@
-fn hote(){
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use crate::preparation::crée_joueur;
+
+pub fn hote(){
+
+    let mut joueur = crée_joueur();
     //crée joueur
+
+    println!("je suis maintenan un hote joueur");
+
+
+    connextion_au_client();
+    println!("Je suis l'hote et tout vas bien !!");
+
 
 
     //si client se connecter et attendre
     //si h proposer la connection et attendre mon bon vouloir
     //si h crée la liste et la partager
+
+}
+
+async  fn connextion_au_client() -> Result<(), Box<dyn std::error::Error>>{
+    let listener = TcpListener::bind("127.0.0.1:9000").await?;
+
+    loop {
+        // Accepter une connexion entrante
+        let (mut socket, addresse) = listener.accept().await?;
+
+        // Gérer la connexion
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            loop {
+                // Lire les données envoyées par le client
+                let n = match socket.read(&mut buf).await {
+                    Ok(0) => {
+                    eprintln!("connexion {} fermée",addresse);
+                    return;
+                    } // Connexion fermée
+                    Ok(n) => n,
+                    Err(_) => {
+                        eprintln!("erreur de lecture du client {}",addresse);
+                        return;
+                    } // Erreur de lecture
+                };
+
+                // Écrire une réponse au client
+                if let Err(_) = socket.write_all(&buf[..n]).await {
+                    return; // Erreur d'écriture
+                }
+            }
+        });
+
+    }
 
 }
--- src/multi_joueur/multi_joueur.rs
+++ src/multi_joueur/multi_joueur.rs
@@ -1,20 +1,28 @@
 use std::io;
 use crate::affichage::affichage::Affichage;
 use crate::affichage::terminal::AffichageTerminal;
+use crate::multi_joueur::hote::hote;
 
 static  affichage: AffichageTerminal = AffichageTerminal;
 pub fn multi_joueur(){
-    let role = choix_role();
+    let role1 = "hote";
+    let role2 = "client";
+    let role = choix_role(&role1, &role2);
     println!("Tu as choisie {}!", role);
+
+    match role.as_str() {
+        r if r == role1 => hote(),
+        //r if r == role2 => client(),
+        _ => println!("Rôle inconnu tu as rentrer : {}", role),
+    }
 
 
 }
 
 
 
-fn choix_role() -> String{  // les roles sont hebergeure ou client
-    let role1 = "hebergeure";
-    let role2 = "client";
+fn choix_role(role1 : &str, role2 : &str) -> String{  // les roles sont hote ou client
+
     affichage.afficher(format!("Role : {} ou {}", role1, role2));
     loop {
         let mut role = String::new();
@@ -22,8 +30,8 @@ fn choix_role() -> String{  // les roles sont hebergeure ou client
             .read_line(&mut role)
             .expect("Erreur lors de la lecture du role");
 
-
-        match role.trim().to_lowercase().as_str() {
+        role = role.trim().to_string();
+        match &role {
             r if r == role1 || r == role2 => return role,
             _ => (),
         }
