2f3be68fdcb3b8316b2ca3c637c2615a714a3319
Clément <lefevrescoazec@gmail.com>
2025-12-13 00:48:12 +0100

    ajout de plusieurs mot

--- src/outils/terminal.rs
+++ src/outils/terminal.rs
@@ -6,7 +6,7 @@ use crate::outils::outils::demander;
 
 pub fn afficher_en_tete(){
     crossterm::execute!(stdout(), crossterm::terminal::Clear(crossterm::terminal::ClearType::All)).unwrap();
-    let en_tete = "\n\nLes mots sont sans majuscule mais avec accent, les locutions latine ne prenne pas de tiret".green();
+    let en_tete = "\n\nLes mots sont sans majuscule (sauf pour les noms propre) mais avec accent, les locutions latine ne prenne pas de tiret".green();
     let indices = "\n\
     {indice/i} pour avoir le nombre de lettres\n\
     {passe/p} pour changer de mot\n\
