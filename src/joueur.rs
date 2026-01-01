0a0c81280491deb76f7a00d6cb01346d294d9f7e
Clément <lefevrescoazec@gmail.com>
2026-01-01 23:07:12 +0100

    un peu d’ajout de doc 2

--- src/joueur.rs
+++ src/joueur.rs
@@ -10,51 +10,152 @@ pub struct Joueur  {
 
 
 impl Joueur {
+    /// Fonction permettant de créer un joueur de base.
+    ///
+    /// # Paramètre
+    /// - Ne prend pas de paramètre.
+    ///
+    /// # Retour
+    /// - Une structure de Joueur.
+    ///
+    /// # Comportement
+    /// Crée un Joueur avec des paramètres de base.
+    ///
     pub fn nouveau() -> Joueur {
     Joueur {bonne_reponse : 0, mauvaise_reponse : 0, question : 0, nom: "".to_string() }
-    } //crée un nouveau Joueur avec les valeurs de base
+    }
 
+
+    /// Fonction renvoyant le nombre de bonnes réponses.
+    ///
+    /// # Paramètre
+    /// - Un &self.
+    ///
+    /// # Retour
+    /// - Le nombre de bonnes réponses sous forme d’usize.
+    ///
     pub fn bonne_reponse(&self) -> usize{
         self.bonne_reponse
-    }  //renvoie le nombre de bonne réponse
+    }
 
+
+    /// Fonction ajoutant une bonne réponse.
+    ///
+    /// # Paramètre
+    /// - Un &mut self.
+    ///
+    /// # Retour
+    /// - Le nombre de bonnes réponses sous forme d’usize.
+    ///
+    /// # Comportement
+    /// Ajoute un au nombre de bonnes réponses,
+    /// renvoie le nombre de bonnes réponses.
+    ///
     pub fn bonne_reponse_aj(&mut self) -> usize{
         self.bonne_reponse +=1;
         self.bonne_reponse
-    }  //ajoute un au nombre de bonne réponse puis le renvoie
+    }
 
+
+    /// Fonction renvoyant le nombre de mauvaises réponse.
+    ///
+    /// # Paramètre
+    /// - Un &self.
+    ///
+    /// # Retour
+    /// - Le nombre de mauvaises réponse sous forme d’usize.
+    ///
     pub fn mauvaise_reponse(&self) -> usize{
         self.mauvaise_reponse
     }  //renvoie le nombre de mauvaise réponse
 
+
+    /// Fonction ajoutant une mauvaise réponse.
+    ///
+    /// # Paramètre
+    /// - Un &mut self.
+    ///
+    /// # Retour
+    /// - Le nombre de mauvaises réponse sous forme d’usize.
+    ///
+    /// # Comportement
+    /// Ajoute un au nombre de mauvaises réponse,
+    /// renvoie le nombre de mauvaises réponse.
+    ///
     pub fn mauvaise_reponse_aj(&mut self) -> usize{
         self.mauvaise_reponse +=1;
         self.mauvaise_reponse
-    }  //ajoute un au nombre de mauvaise réponse puis le renvoie
+    }
 
+
+    /// Fonction renvoyant le nombre de questions répondu.
+    ///
+    /// # Paramètre
+    /// - Un &self.
+    ///
+    /// # Retour
+    /// - Le nombre de questions répondu sous forme d’usize.
+    ///
     pub fn question(&self) -> usize{
         self.question
-    }  //renvoie le nombre de question répondue
+    }
 
     pub fn question_suivante(&mut self) -> usize{
         self.question += 2;
         self.question
     }  //augmente question de deux pour accéder a la question suivante
 
+
+
     pub fn fin(&self, manche:usize) -> bool {
         self.question / 2 == manche
     } //renvoie true si on a fais manche tour
 
 
+    /// Fonction servant à définir le nom du joueur.
+    ///
+    /// # Paramètre
+    /// - Un &mut self,
+    /// - un nom sous forme de string.
+    ///
+    /// # Retour
+    /// - Ne retourne rien.
+    ///
+    /// # Comportement
+    ///Remplace le nom du joueur par celui passer en paramètre.
+    ///
     pub fn défini_nom(&mut self, nom: String){
         self.nom = nom;
     }
 
 
+    /// Fonction renvoyant le nom du joueur.
+    ///
+    /// # Paramètre
+    /// - Un &self.
+    ///
+    /// # Retour
+    /// - Le nom sous forme d’usize.
+    ///
+    /// # Comportement
+    /// Clone le nom et le renvoie.
+    ///
     pub fn nom(&self) -> String{
         self.nom.clone()
     }
 
+
+    /// Fonction remettant le nombre de questions à zéro.
+    ///
+    /// # Paramètre
+    /// - Un &mut self.
+    ///
+    /// # Retour
+    /// - Ne retourne rien.
+    ///
+    /// # Comportement
+    /// Définie le nombre de questions répondu à zéro.
+    ///
     pub fn remet_les_questions_a_zero(&mut self){
         self.question = 0;
     }
