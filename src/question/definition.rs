f60b0aedb992294465c5ae45d87ab05a774292f4
Clément <lefevrescoazec@proton.me>
2026-01-22 18:10:37 +0100

    ajout des commentaires dans definition.rs | correction de petite erreurs dans definition

--- src/question/definition.rs
+++ src/question/definition.rs
@@ -1,4 +1,4 @@
-
+use std::fs;
 
 
 pub struct Definition{
@@ -7,27 +7,53 @@ pub struct Definition{
 }
 
 impl Definition{
+
+    /// Fonction de création d'un nouvel itérateur de [Definition]
+    ///
+    /// # Paramètre 
+    /// - Prend en paramètre un chemin de fichier
+    ///
+    /// # Retour
+    /// - Retourne un itérateur de question
+    ///
+    /// # Comportement
+    /// - Crée un itérateur de définition tiré du fichier passer en paramètre
     fn nouveau(fichier: &str){
         Definition{
-        liste = fs::read_to_string(fichier)
+        liste : fs::read_to_string(fichier)
             .expect("Je n'arrive pas a lire la définition")
             .lines()
+            .split(":")
             .skip(1)
             .step_by(2)
             .map(|v| v.to_string())
-            .collect::Vec<String>(),
-        curseur = 0;
+            .collect::<Vec<String>>(),
+        curseur : 0
         }
     }
 }
 
+/// Ajout du trait [Iterator] à la structure [Definition]
 impl Iterator for Definition{
+    /// L'itérateur renvoi des String
     type Item = String;  //type de retour de l'itérateur
 
+    /// Fonction itérant l'itérateur
+    ///
+    /// # Paramètre
+    /// - Prend en paramètre un [Definition] mutable
+    ///
+    /// # Retour 
+    /// - Une option, contenant la définition
+    ///
+    /// # Comportement
+    /// Clone la définition de la liste déterminer grâce au curseur
+    /// incrémente le curseur
+    /// renvoi la définition
     fn next(&mut self)-> Option<Self::Item>{
         if self.curseur < self.liste.len(){
             let def = self.liste[self.curseur].clone();
-            curseur += 1;
+            self.curseur += 1;
             Some(def)
         }
         else{
